ea44b71c734cdcd828029c27258693f373504b2f
hjck4433 <hjck4433@gmail.com>
2023-11-06 16:30:16 +0900

    현지 - Join 초기화

--- src/style/Joinstyle.jsx
+++ src/style/Joinstyle.jsx
@@ -1,47 +1,5 @@
 import styled from "styled-components";
 
-const Joinstyle = styled.div`
-  margin: 50px;
-  display: grid;
-  justify-content: center;
-  align-items: center;
-
-  .signup {
-    text-align: center;
-    height: 80px;
-    padding: 20px;
-  }
-  .profileImg {
-    width: 150px;
-    height: 150px;
-    border-radius: 50%;
-    border: 1px solid #999;
-  }
-  label {
-    margin: 0 auto;
-    width: 100px;
-    height: 400px;
-    text-align: center;
-  }
-  input {
-    width: 200px;
-    height: 25px;
-  }
-  button {
-    width: 70px;
-  }
-  @media only screen and (max-width: 768px) {
-    .container {
-      width: 90vw;
-      height: 80px;
-
-      display: block;
-      flex-grow: 0;
-      h2 {
-        font-size: 2em;
-      }
-    }
-  }
-`;
+const Joinstyle = styled.div``;
 
 export default Joinstyle;
