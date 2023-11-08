fdb5472af7a2017624f55a257ebc550d08152d28
ljw2669 <dlwodnnjs@gmail.com>
2023-11-08 09:58:49 +0900

    이재원 UserInfo수정

--- src/component/UserInfo.jsx
+++ src/component/UserInfo.jsx
@@ -1,71 +1,5 @@
-import styled from "styled-components";
-import { Link } from "react-router-dom";
-import 기본프로필 from "../images/기본프로필.jpg";
-
-const UserInfoContainer = styled.div`
-  width: 100%;
-  /* background-color: var(--MIDBLUE); */
-  /* height: 2000px; */
-  .container {
-    position: relative;
-    display: flex;
-    justify-content: space-between;
-    height: auto;
-    flex-wrap: wrap;
-    padding: 10px 0;
-
-    .userProfile {
-      width: 40%;
-      background-color: red;
-      display: flex;
-      flex-direction: column; /* 세로로 쌓이도록 설정 */
-      align-items: center; /* 가운데 정렬을 위해 추가 */
-      justify-content: center; /* 가운데 정렬을 위해 추가 */
-      .profileImg {
-        position: relative;
-        width: 100%;
-        padding-bottom: 100%; /* 이미지의 높이를 부모 요소와 동일하게 설정 */
-        overflow: hidden;
-        .img {
-          position: absolute;
-          width: 100%;
-          height: 100%; /* 이미지의 높이를 부모 요소와 동일하게 설정 */
-          object-fit: cover;
-          top: 0;
-        }
-      }
-    }
-    .userContent {
-      display: flex;
-      width: 60%;
-      flex-direction: column; /* 세로로 쌓이도록 설정 */
-      /* align-items: center; 가운데 정렬을 위해 추가 */
-      justify-content: center; /* 가운데 정렬을 위해 추가 */
-
-      /* background-color: green; */
-      .userBox {
-        display: flex;
-        align-items: center;
-        margin-bottom: 20px;
-        .title {
-          width: 120px;
-          font-size: 1.2em;
-          font-weight: 800;
-        }
-      }
-    }
-    .userModification {
-      position: absolute;
-      right: 0;
-    }
-    @media only screen and (max-width: 768px) {
-      .userModification {
-        bottom: 0;
-      }
-    }
-  }
-`;
-
+import UserInfoComp from "../style/UserInfoStyle";
+import { Link } from "react-router-dom/dist";
 const UserInfo = () => {
   const userInfoEx = {
     name: "송중기",
@@ -79,10 +13,10 @@ const UserInfo = () => {
 
   return (
     <>
-      <UserInfoContainer>
+      <UserInfoComp>
         <div className="container">
           <div className="userProfile">
-            <h3>{userInfoEx.name}님, 안녕하세요!</h3>
+            <h2>{userInfoEx.name}님, 안녕하세요!</h2>
             <div className="profileImg">
               {<img src={userInfoEx.userUrl} alt="송중기" />}
             </div>
@@ -95,15 +29,15 @@ const UserInfo = () => {
             <div className="userBox">
               <p className="title">아이디</p>
               <p>{userInfoEx.userId}</p>
-            </div>{" "}
+            </div>
             <div className="userBox">
               <p className="title">EMAIL</p>
               <p>{userInfoEx.email}</p>
-            </div>{" "}
+            </div>
             <div className="userBox">
               <p className="title">전화번호</p>
               <p>{userInfoEx.phoneNumber}</p>
-            </div>{" "}
+            </div>
             <div className="userBox">
               <p className="title">소재지</p>
               <p>{userInfoEx.addr}</p>
@@ -116,7 +50,7 @@ const UserInfo = () => {
             </Link>
           </div>
         </div>
-      </UserInfoContainer>
+      </UserInfoComp>
     </>
   );
 };
